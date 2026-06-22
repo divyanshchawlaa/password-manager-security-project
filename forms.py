1151b1dfa618c3c1f2ca0eadeca57f02806f53c2
divyanshchawlaa <divyanshchawlaa@gmail.com>
2026-06-23 01:48:41 +0200

    Add password strength validation

--- forms.py
+++ forms.py
@@ -1,13 +1,49 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField
-from wtforms import PasswordField
-from wtforms import SubmitField
+from wtforms import (
+    StringField,
+    PasswordField,
+    SubmitField
+)
 
 from wtforms.validators import (
     DataRequired,
     Email,
-    Length
+    Length,
+    ValidationError
 )
+
+import re
+
+
+def strong_password(form, field):
+
+    password = field.data
+
+    if len(password) < 8:
+        raise ValidationError(
+            "Password must be at least 8 characters."
+        )
+
+    if not re.search(r"[A-Z]", password):
+        raise ValidationError(
+            "Password must contain an uppercase letter."
+        )
+
+    if not re.search(r"[a-z]", password):
+        raise ValidationError(
+            "Password must contain a lowercase letter."
+        )
+
+    if not re.search(r"\d", password):
+        raise ValidationError(
+            "Password must contain a number."
+        )
+
+    if not re.search(r"[!@#$%^&*(),.?\":{}|<>]", password):
+        raise ValidationError(
+            "Password must contain a special character."
+        )
+
 
 class RegisterForm(FlaskForm):
 
@@ -23,10 +59,14 @@ class RegisterForm(FlaskForm):
 
     password = PasswordField(
         "Password",
-        validators=[DataRequired(), Length(min=8)]
+        validators=[
+            DataRequired(),
+            strong_password
+        ]
     )
 
     submit = SubmitField("Register")
+
 
 class LoginForm(FlaskForm):
 
@@ -41,6 +81,7 @@ class LoginForm(FlaskForm):
     )
 
     submit = SubmitField("Login")
+
 
 class CredentialForm(FlaskForm):
 
